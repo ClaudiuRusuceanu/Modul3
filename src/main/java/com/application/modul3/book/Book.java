403eef21d4085c831211c6b59c8a715356bbc7a0
Bearzil <95644795+Bearzil@users.noreply.github.com>
2022-01-19 15:20:38 +0200

    first

---
+++ src/main/java/com/application/modul3/book/Book.java
@@ -0,0 +1,60 @@
+package com.application.modul3.book;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "book", schema = "administration")
+public class Book {
+	@Id
+	@Column(name = "id")
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+
+	@Column(name = "title")
+	private String title;
+
+	@Column(name = "year")
+	private int year;
+
+	@Column(name = "cod")
+	private String cod;
+
+	protected Integer getId() {
+		return id;
+	}
+
+	protected void setId(Integer id) {
+		this.id = id;
+	}
+
+	protected String getTitle() {
+		return title;
+	}
+
+	protected void setTitle(String title) {
+		this.title = title;
+	}
+
+	protected int getYear() {
+		return year;
+	}
+
+	protected void setYear(int year) {
+		this.year = year;
+	}
+
+	protected String getCod() {
+		return cod;
+	}
+
+	protected void setCod(String cod) {
+		this.cod = cod;
+	}
+	
+	
+}
---
+++ src/main/java/com/application/modul3/book/BookController.java
@@ -0,0 +1,41 @@
+package com.application.modul3.book;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/carti")
+public class BookController {
+	@Autowired
+	private BookService bookService;
+
+	@PostMapping
+	public Book createCarte(@RequestBody Book book) {
+		return bookService.createBook(book);
+	}
+
+	@GetMapping("/bookList")
+	public List<Book> getAllBooks() {
+		return bookService.getAllBooks();
+	}
+
+	@GetMapping("/{id}")
+	public Book getBookById(@PathVariable Integer id) {
+		return bookService.getBookById(id);
+
+	}
+
+	@DeleteMapping("/{id}")
+	public void deleteBookById(@PathVariable Integer id) {
+		bookService.deleteBookById(id);
+	}
+
+}
